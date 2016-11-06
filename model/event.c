dd387355ec6d7622bbe4326966b29a9053e56c04
Sándor Balázs <Sndor Balzs>
2016-11-06 21:34:28 +0100

    Az Event adatszerkezet elkészült.

---
+++ model/event.c
@@ -0,0 +1,110 @@
+/*
+    Tárgy: Elte IK Operációs rendszerek 1. Beadandó
+    Készítette: Sándor Balázs - AZA6NL
+    Leadás: 2016.11.07
+
+    Az eseményeket reprezentáló adatszerkezetethez tartozó fügvények.
+*/
+
+//mallochoz szügséges
+#include <stdlib.h>
+//strcpy-hez
+#include <string.h>
+#include "event.h"
+#include "visitor.h"
+
+//Új üres esemény
+struct Event* newEvent(int id, char* name)
+{
+	struct Event* _this = malloc(sizeof(struct Event));
+	_this->id = id;
+	strcpy(_this->name, name);
+	_this->size = 0;
+	_this->_realSize = defaultArraySize;
+	Visitor**  _eventArray = malloc(_this->_realSize * sizeof(Visitor*));
+	_this->eventArray = _eventArray;
+	return _this;
+}
+
+
+//Látogató hozzáadását teszi lehetőve az eseményhez
+/*	Ha a tömb betelt, akkor dupla méretűt deklarálunk, átmásoljuk az adatokat
+ és a régit töröljük a C++ STD Vector mintájára.*/
+int addVisitorToEvent(Event* event, Visitor* visitor)
+{
+	int i;
+	
+	if(event->_realSize > event->size)
+	{
+		event->eventArray[event->size] = visitor;
+		event->size++;
+	}
+	else
+	{
+		event->_realSize *= 2;
+		Visitor**  _eventArray = malloc(event->_realSize * sizeof(Visitor*));
+		for(i = 0; i < event->size; ++i)
+		{
+			_eventArray[i] = event->eventArray[i];
+		}
+
+		/*Felszabadítjuk a címeket tároló tömböt,
+		de az elemeket nem. Azok címeit az új tömb már tárolja.*/
+		Visitor** arr = lines -> eventArray;
+		free(arr);
+
+		//A helyérekerül az új nagyobb tömb címe.
+		lines -> eventArray = _eventArray;
+		
+		//Elvégezzük a beszúrást
+		event->eventArray[event->size] = visitor;
+		event->size++;
+	}
+	return 0;
+}
+
+
+//Látogató törlése az eseményről
+int deleteVisitorFromEvent(Event* event, Visitor* visitor);
+{
+	int i, l = 0;
+	Visitor**  _eventArray = malloc(event -> _realSize * sizeof(Visitor*));
+	for(i = 0; i < event->size; ++i)
+	{
+		if(event->eventArray[i] != visitor)
+		{
+			_eventArray[l] = lines->eventArray[i];
+		}
+		else
+		{
+			l = i-1;
+			freeVisitor(event->eventArray[i]);
+		}
+		++l;
+	}
+	Visitor** arr = event -> eventArray;
+	free(arr);
+	event -> eventArray = _eventArray;
+	event->size--;
+	return 0;
+}
+
+
+//Index szerinti keresés a látogatók között
+struct Visitor* getVisitorFromEventById(Event* event, int id){
+	return event->eventArray[id];
+}
+
+/*Az Event típus destruktora
+Kell destrukfor, mivel az eventArray külön álló egységet képez a memóriában
+Törli a tömb által mutatott elemeket is!*/
+void freeEvent(Event* event)
+{
+	int i;
+	for(i = 0; i < event->size; ++i)
+	{
+		freeVisitor(event->lineArray[i]);
+	}
+	free(event->eventArray);
+	free(event);
+}
---
+++ model/event.h
@@ -0,0 +1,82 @@
+/*
+    Tárgy: Elte IK Operációs rendszerek 1. Beadandó
+    Készítette: Sándor Balázs - AZA6NL
+    Leadás: 2016.11.07
+
+    Az eseményeket reprezentáló adatszerkezetet tartalmazó fejléc.
+*/
+
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <stdio.h>
+#include "visitor.h"
+
+//A eseményt modellező adatszerkezet
+/*
++------------------------+
+|          Event         |
+| +--------------------+ |
+| |    int id          | |
+| +--------------------+ |
+| +--------------------+ |
+| |    char* name      | |
+| +--------------------+ |
+| +--------------------+ |
+| |    int size        | |
+| +--------------------+ |
+| +--------------------+ |
+| |    int _realSize   | |
+| +--------------------+ |
+| +--------------------+ |            +--------------------+
+| |Visitor** eventArray| |===========>|    eventArray[]    |
+| +--------------------+ |            | +----------------+ |              +----------------+
++------------------------+            | |     Visitor*   | | ===========> |     Visitor    |
+                                      | +----------------+ |              +----------------+
+                                      | +----------------+ |              +----------------+
+                                      | |     Visitor*   | | ===========> |     Visitor    |
+                                      | +----------------+ |              +----------------+
+                                      | +----------------+ |              +----------------+
+                                      | |     Visitor*   | | ===========> |     Visitor    |
+                                      | +----------------+ |              +----------------+
+                                      |         ...        |                      ...
+                                      +--------------------+
+*/
+
+typedef struct Event
+{
+    //A rendezvény sorszáma
+    int id;
+    //A rendezvény neve
+    char name[100];
+	//Felvett uticélok száma
+	int size;
+	//A tároló tömb valós mérete
+	int _realSize;
+	//Járatokra mutató pointerek tömbje
+	Visitor** eventArray;
+}Event;
+
+//Az alapértelmezett tároló tömb mérete
+static const int defaultArraySize = 5;
+
+//Új üres járatlista létrehozásához
+struct Event* newEvent(int, char*);
+
+//Látogató hozzáadását teszi lehetőve az eseményhez
+/*	Ha a tömb betelt, akkor dupla méretűt deklarálunk, átmásoljuk az adatokat
+ és a régit töröljük a C++ STD Vector mintájára.*/
+int addVisitorToEvent(Event*, Visitor*);
+
+//Látogató törlése az eseményről
+int deleteVisitorFromEvent(Event*, Visitor*);
+
+//Index szerinti keresés a látogatók között
+struct Visitor* getVisitorFromEventById(Event*, int);
+
+/*Az Event típus destruktora
+Kell destrukfor, mivel az eventArray külön álló egységet képez a memóriában
+Törli a tömb által mutatott elemeket is!*/
+void freeEvent(Event*);
+
+#endif
