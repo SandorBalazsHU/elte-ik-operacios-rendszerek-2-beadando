679d8e3c663b9079df5075639ea2ef1f9d7f7f55
Sándor Balázs <Sndor Balzs>
2016-11-06 23:00:40 +0100

    A Visitor.h elkészülte

--- model/visitor.h
+++ model/visitor.h
@@ -6,3 +6,49 @@
 
     A látogatókat reprezentáló adatszerkezetet tartalmazó fejléc.
 */
+
+#ifndef VISITOR_H
+#define VISITOR_H
+
+#include <stdio.h>
+#include <time.h>
+
+//A látogatókat modellező adatszerkezet
+/*
++--------------------------------+
+|             Visitor            |
+|     +--------------------+     |
+|     | int id             |     |
+|     +--------------------+     |
+|     +--------------------+     |
+|     | char* name         |     |
+|     +--------------------+     |
+|     +--------------------+     |
+|     | char* mail         |     |
+|     +--------------------+     |
+|     +--------------------+     |
+|     | time_t date        |     |
+|     +--------------------+     |
+|                                |
++--------------------------------+ 
+*/
+
+typedef struct Visitor
+{
+    //A látogató sorszáma
+    int id;
+    //A látogató neve
+    char name[100];
+    //A látogató e-mail címe
+    char email[100];
+    //Regisztáció időpontja
+    time_t date;
+}Visitor;
+
+//Új látogató létrehozása
+struct Visitor* newVisitor(int, char*, char*, time_t);
+
+//A Visitor típus destruktora
+void freeVisitor(Visitor*);
+
+#endif
