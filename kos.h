f8cc0fa7eb5f2d570ac3a72571074c41ec320da4
Sándor Balázs <Sndor Balzs>
2016-11-07 11:11:21 +0100

    Kos.h befejezése.

--- kos.h
+++ kos.h
@@ -4,7 +4,21 @@
     Leadás: 2016.11.07
 	Git: https://github.com/SandorBalazsHU/elte-ik-os-1
 
-    Egy egységes API-t biztosít.
+    Egy egységes API-t biztosít. Így csak ezt az egy fejlécet kell használni.
 */
 
+//A konzolkezelő függvények gyűjteménye
 #include "./view/ConsoleIO.h"
+
+//Az eseménytároló adatszerkezet és a hozzá tartozó függvények gyűjteménye.
+#include "./model/Events.h"
+
+/*Az eseményt reprezentáló szerkezet és a hozzá tartozó függvények gyűjteménye.
+Ez tárolja a látogatókat is.*/
+#include "./model/Event.h"
+
+//A látogatókat reprezentáló szerkezet és a hozzá tartozó függvények gyűjteménye.
+#include "./model/Visitor.h"
+
+//A fájlműveletekért felelős függvények gyűjteménye
+#include "./controller/File.h"
