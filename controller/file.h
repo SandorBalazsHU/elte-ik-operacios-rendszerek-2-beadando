4178161b1618f0bcab98412e5cb0092087f1cba0
Sándor Balázs <sabuaai@inf.elte.hu>
2016-12-07 18:43:59 +0100

    Delete file.h

--- controller/file.h
+++
@@ -1,7 +0,0 @@
-/*
-    Tárgy: Elte IK Operációs rendszerek 1. Beadandó
-    Készítette: Sándor Balázs - AZA6NL
-	Git: https://github.com/SandorBalazsHU/elte-ik-os-1
-
-    A fájlkezelésért felelős függvények
-*/
